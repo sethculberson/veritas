3eb3b6e48ced912117a3588a02c6f53175349550
Fynn Buesnel <fynnbuesnel@gmail.com>
2025-10-04 10:43:16 -0400

    Services Folder

--- backend/app.py
+++ backend/app.py
@@ -7,6 +7,6 @@ CORS(app)  # allow requests from React
 @app.route("/api/hello")
 def hello():
     return jsonify({"message": "Hello from Flask!"})
-
+    
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
