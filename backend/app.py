88098f72cc794561ee5f5f8ca6831f81416404ac
Fynn Buesnel <fynnbuesnel@gmail.com>
2025-10-04 21:51:08 -0400

    Docker Deployment

--- backend/app.py
+++ backend/app.py
@@ -10,10 +10,7 @@ CORS(app)  # allow requests from React
 app.register_blueprint(autofill_bp)  # register autofill blueprint
 app.register_blueprint(getInfo_bp)  # register getInfo blueprint
 
-
-@app.route("/api/hello")
-def hello():
-    return jsonify({"message": "Hello from Flask!"})
-
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
